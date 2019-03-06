49721b57c3a204dca9b95cdc088496d26e3ba177
Adrian Haynes <adrianhaynes@Adrians-MacBook-Pro.local>
2019-03-06 08:46:39 -0600

    configue in memory data service

--- src/app/character/sidebar/sidebar.component.ts
+++ src/app/character/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
+import { Character } from '../../character';
+import { CharacterService } from '../../character.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -10,10 +13,15 @@ export class SidebarComponent implements OnInit {
   @Input() character;
   characterID;
 
-  constructor() { }
+  constructor(private characterService: CharacterService) { }
 
   ngOnInit() {
     this.characterID = this.character;
+  }
+
+  save(): void {
+    this.characterService.updateCharacter(this.character)
+      .subscribe(() => this.goBack());
   }
 
 }
